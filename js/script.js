06d8ee020405fb35458045656ad2854a8ffb686a
Zaur Saidov <saidov1991@icloud.com>
2024-07-28 01:38:55 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -1,42 +1,40 @@
-const details = document.querySelector('.details-arrow');
+const details = document.querySelector('.details_arrow');
 const btnTest = document.querySelector('.btn-test');
-const menuCover = document.querySelector('.menu');
-const menuBurger = document.querySelector('.sandwich');
-const menuMainBtn = document.querySelector('.menu-list__item_main');
-const menuTestItem = document.querySelector('.menu-list__item_test');
-const exiteBtn = document.querySelector('.menu_exit-btn');
-const testBtn = document.querySelector('.btn-test');
 // const contentHidden = document.querySelector('.main-page__content');
+const menuCover = document.querySelector('.menu');
+const menuBtn = document.querySelector('.sandwich');
+const menuMainBtn = document.querySelector('.menu-list__item_main');
+const menuTestBtn = document.querySelector('.menu-list__item_test');
+const exitBtn = document.querySelector('.menu_exit-btn');
+const testBtn = document.querySelectorAll('.btn-test');
 
-//details.addEventListener('click',showHideDetails);
-menuBurger = document.addEventListener('click', showHideMenu);
-exiteBtn = document.addEventListener('click', showHideMenu);
-btnTest.forEach(btn => (
+// details.addEventListener('click', showHideDetails);
+menuBtn.addEventListener('click', showHideMenu);
+exitBtn.addEventListener('click', showHideMenu);
+testBtn.forEach(btn => (
     btn.addEventListener('click', function () {
-        window.location.href = `https://takethisworld.github.io/IQ-Test/questions/question.html`
+        window.location.href = `https://takethisworld.github.io/IQ-Test/questions/question.html`;
     })
-));
+))
 menuMainBtn.addEventListener('click', function () {
     window.location.href = `https://takethisworld.github.io/IQ-Test/index.html`;
-});
-menuTestItem.addEventListener('click', function () {
-    window.location.href = `https://takethisworld.github.io/IQ-Test/questions/question.html`
-});
+})
+menuTestBtn.addEventListener('click', function () {
+    window.location.href = `https://takethisworld.github.io/IQ-Test/questions/question.html`;
+})
 
 // function showHideDetails() {
-//     contentHidden.classList.toogle('hidden');
-//     if (contentHidden.classList.contains('hidden')) {
-//         details.classList.add('arrow-up');
-//     } else {
-//         details.classList.remove('arrow-up');
-//     }
+//     contentHidden.classList.toggle('hidden')
+//     if (!contentHidden.classList.contains('hidden')) {
+//         details.classList.add('arrow-up')
+//     } else details.classList.remove('arrow-up')
 // }
 
 function showHideMenu() {
     menuCover.classList.toggle('show-menu');
-    // if (contentHidden.classList.conteins('hidden')) {
-    //     contentHidden.classList.add('hidden');
-    // } else {
-    //     contentHidden.classList.remove('arrow-up')
+    // if (!contentHidden.classList.contains('hidden')) {
+    //     contentHidden.classList.add('hidden')
+    //     details.classList.remove('arrow-up')
     // }
 }
+
