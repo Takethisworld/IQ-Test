815f48f1328ad4b1d607352272835f2514fb0e08
Zaur Saidov <saidov1991@icloud.com>
2024-07-28 00:57:36 +0500

    fix

--- js/script.js
+++ js/script.js
@@ -4,33 +4,33 @@ const menuCover = document.querySelector('.menu');
 const menuBurger = document.querySelector('.sandwich');
 const menuMainBtn = document.querySelector('.menu-list__item_main');
 const menuTestItem = document.querySelector('.menu-list__item_test');
-const exiteBtn = document.querySelector('.exite-btn');
+const exiteBtn = document.querySelector('.menu_exit-btn');
 const testBtn = document.querySelector('.btn-test');
 // const contentHidden = document.querySelector('.main-page__content');
 
 //details.addEventListener('click',showHideDetails);
 menuBurger = document.addEventListener('click', showHideMenu);
 exiteBtn = document.addEventListener('click', showHideMenu);
-testBtn.forEach(btn => (
+btnTest.forEach(btn => (
     btn.addEventListener('click', function () {
-        window.location.href = `https://takethisworld.github.io/IQ-Test/question/question.html`
+        window.location.href = `https://takethisworld.github.io/IQ-Test/questions/question.html`
     })
 ));
 menuMainBtn.addEventListener('click', function () {
     window.location.href = `https://takethisworld.github.io/IQ-Test/index.html`;
 });
 menuTestItem.addEventListener('click', function () {
-    window.location.href = `https://takethisworld.github.io/IQ-Test/quesions/question.html`
+    window.location.href = `https://takethisworld.github.io/IQ-Test/questions/question.html`
 });
 
-function showHideDetails() {
-    contentHidden.classList.toogle('hidden');
-    if (contentHidden.classList.contains('hidden')) {
-        details.classList.add('arrow-up');
-    } else {
-        details.classList.remove('arrow-up');
-    }
-}
+// function showHideDetails() {
+//     contentHidden.classList.toogle('hidden');
+//     if (contentHidden.classList.contains('hidden')) {
+//         details.classList.add('arrow-up');
+//     } else {
+//         details.classList.remove('arrow-up');
+//     }
+// }
 
 function showHideMenu() {
     menuCover.classList.toggle('show-menu');
